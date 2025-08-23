3cb10b3b5caee04d2acff4000846bf2da9c087f8
Michael <edwardmichael953@gmail.com>
2025-08-23 23:04:37 +0100

    still going

--- java/MenstrualApp.java
+++ java/MenstrualApp.java
@@ -1,10 +1,14 @@
 import java.util.Scanner;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 
+
 public class MenstrualApp{
-Scanner input = new Scanner(System.in);
+
 	public static void main(String... arg){
+
+Scanner input = new Scanner(System.in);
 
 System.out.println("Enter last menstruation starting date(Date/Month/Year): ");
 String lastMenstruation = input.next();
@@ -13,25 +17,30 @@ System.out.println("Enter your cycle Length: ");
 int cycleLength = input.nextInt();
 
 System.out.println("Enter your period Length: ");
-int cycleLength = input.nextInt();
+int periodLength = input.nextInt();
 
-System.out.print(calculateNextPeriod())
+System.out.print(calculateNextPeriod(lastMenstruation, cycleLength));
 	}
 
-public static int calculateNextPeriod(LocalDate lastPeriodDate, int cycleLength){
-
-	date.plusDays(cycleLength);
-}
-
-public static int calculateOvulation(){
 
 
-}
-
-public static int getSafePeriods(){
-
+public static LocalDate calculateNextPeriod(String lastPeriodDate, int cycleLength){
+	
+DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy"):
+	LocalDate date = LocalDate.parse(lastPeriodDate, formatter);
 
 }
+
+
+//public static int calculateOvulation(){
+
+
+//}
+
+//public static int getSafePeriods(){
+
+
+//}
 
 
 }

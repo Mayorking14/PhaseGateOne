8fd5ec01a4e74930f6b0e4fba817307f8d79094f
Michael <edwardmichael953@gmail.com>
2025-08-23 19:12:00 +0100

    progress

--- java/MenstrualApp.java
+++ java/MenstrualApp.java
@@ -1,22 +1,24 @@
 import java.util.Scanner;
-
-import java.time.LocalDate
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 public class MenstrualApp{
 Scanner input = new Scanner(System.in);
 	public static void main(String... arg){
 
-System.out.println("Enter last menstruation starting date(year/month/day): ");
+System.out.println("Enter last menstruation starting date(Date/Month/Year): ");
 String lastMenstruation = input.next();
 
-System.out.println("Enter your period cycleLength: ");
+System.out.println("Enter your cycle Length: ");
 int cycleLength = input.nextInt();
 
+System.out.println("Enter your period Length: ");
+int cycleLength = input.nextInt();
 
-
+System.out.print(calculateNextPeriod())
 	}
 
-public static int calculateNextPeriod(LocalDate date, int cycleLength){
+public static int calculateNextPeriod(LocalDate lastPeriodDate, int cycleLength){
 
 	date.plusDays(cycleLength);
 }
